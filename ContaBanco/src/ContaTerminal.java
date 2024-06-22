9ab2a9e345b57e039f0ce697ac28ba768e28c639
stephanie-hcruz <stefaniefernanda1234@hotmail.com>
2024-06-22 16:53:35 -0300

    ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite sua Conta:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite sua Agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu Nome:");
+        String nome = scanner.next();
+
+        System.out.println("Digite o saldo que deseja sacar:");
+        double saldo = scanner.nextDouble(); 
+
+        System.out.println("Olá " + nome + " " + "obrigado por criar uma conta em nosso banco");
+        System.out.println("Sua agência é " + agencia + " " + "e sua conta " + numero + " "); 
+        System.out.println("Seu saldo de " + saldo + " " + "já está disponível para saque.");
+    
     }
+
 }
